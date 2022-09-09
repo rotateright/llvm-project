d0949ef6795432d164171cbe1b1fad3a60d49c1f
Sanjay Patel <spatel@rotateright.com>
2022-09-09 14:59:12 -0400

    [InstCombine] add multi-use add demand fold and remove general knownbits calc

--- llvm/lib/Transforms/InstCombine/InstCombineSimplifyDemanded.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineSimplifyDemanded.cpp
@@ -1064,6 +1064,30 @@ Value *InstCombinerImpl::SimplifyMultipleUseDemandedBits(
 
     break;
   }
+  case Instruction::Add: {
+    unsigned NLZ = DemandedMask.countLeadingZeros();
+    APInt DemandedFromOps = APInt::getLowBitsSet(BitWidth, BitWidth - NLZ);
+
+    // If an operand adds zeros to every bit below the highest demanded bit,
+    // that operand doesn't change the result. Return the other side.
+    computeKnownBits(I->getOperand(1), RHSKnown, Depth + 1, CxtI);
+    if (DemandedFromOps.isSubsetOf(RHSKnown.Zero))
+      return I->getOperand(0);
+
+    computeKnownBits(I->getOperand(0), LHSKnown, Depth + 1, CxtI);
+    if (DemandedFromOps.isSubsetOf(LHSKnown.Zero))
+      return I->getOperand(1);
+//
+//    // Compute the Known bits to simplify things downstream.
+//    computeKnownBits(I, Known, Depth, CxtI);
+//
+//    // If this user is only demanding bits that we know, return the known
+//    // constant.
+//    if (DemandedMask.isSubsetOf(Known.Zero|Known.One))
+//      return Constant::getIntegerValue(ITy, Known.One);
+
+    break;
+  }
   case Instruction::AShr: {
     // Compute the Known bits to simplify things downstream.
     computeKnownBits(I, Known, Depth, CxtI);
