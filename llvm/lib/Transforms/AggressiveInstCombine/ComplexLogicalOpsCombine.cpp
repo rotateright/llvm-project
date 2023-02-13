6c5a32dad803877ad8125e209bd7cf96a9fc9a1b
Sanjay Patel <spatel@rotateright.com>
2023-02-13 14:43:50 -0500

    [ComplexLogicCombine 1/?] Implement a general way to simplify complex logical operations. new files from: https://reviews.llvm.org/D142803

---
+++ llvm/lib/Transforms/AggressiveInstCombine/ComplexLogicalOpsCombine.cpp
@@ -0,0 +1,215 @@
+//===-------- ComplexLogicalOpsCombine.cpp -------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file help to find the simplest expression for a complex logic
+// operation chain. We canonicalize all other ops to and/xor.
+// For example:
+//    a | b --> (a & b) ^ a ^ b
+//    c ? a : b --> (c & a) ^ ((c ^ true) & b)
+// We use a unsigned set to represent the expression. Every value that is not
+// comes from logic operation should be the leaf node. Leaf node is 1 bit in
+// the unsigned value. For example, we have source a, b, c. The mask for a is
+// 1, b is 2 ,c is 4.
+//     a & b & c --> {7}
+//     a & b ^ c & a --> {3, 5}
+//     a & b ^ c & a ^ b --> {3, 5, 2}
+// Every unsigned value is an and chain. The unsigned set is a xor chain.
+// Based on boolean ring, We can treat & as ring multiplication and ^ as ring
+// addition. After that, any logic value can be represented by a unsigned set.
+// For example:
+//     r1 = (a | b) & c -> r1 = (a * b * c) + (a * c) + (b * c) -> {7, 5, 3}
+// Final we need to rebuild the simplest pattern from the expression. For now,
+// we only simplify the code when the expression is leaf or null.
+//
+// Reference: https://en.wikipedia.org/wiki/Boolean_ring
+//
+//===----------------------------------------------------------------------===//
+
+#include "ComplexLogicalOpsCombine.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/Debug.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "aggressive-instcombine"
+
+STATISTIC(NumComplexLogicalOpsSimplified,
+          "Number of complex logical operations simplified");
+
+static cl::opt<unsigned> MaxLogicOpLeafsToScan(
+    "aggressive-instcombine-max-logic-op-leafs", cl::init(8), cl::Hidden,
+    cl::desc("Max leafs of logic ops to scan for aggressive instcombine."));
+
+static cl::opt<unsigned> MaxDepthLogicOpsToScan(
+    "aggressive-instcombine-max-depth-logic-ops", cl::init(8), cl::Hidden,
+    cl::desc("Max depth of logic ops to scan for aggressive instcombine."));
+
+void LogicalOpNode::printValue(raw_ostream &OS, Value *Val) const {
+  if (auto *ConstVal = dyn_cast<Constant>(Val))
+    OS << *ConstVal;
+  else
+    OS << Val->getName();
+}
+
+void LogicalOpNode::printAndChain(raw_ostream &OS, uint64_t Mask) const {
+  if (Mask == LogicalExpr::ExprAllOne) {
+    OS << "-1";
+    return;
+  }
+
+  unsigned MulElementCnt = llvm::popcount(Mask);
+  if (((Mask & LogicalExpr::ExprZero) != 0) || MulElementCnt == 0)
+    return;
+
+  if (MulElementCnt == 1) {
+    printValue(OS, Helper->LeafValues[llvm::Log2_64(Mask)]);
+    return;
+  }
+
+  unsigned MaskIdx;
+  for (unsigned I = 1; I < MulElementCnt; I++) {
+    MaskIdx = llvm::countTrailingZeros(Mask);
+    printValue(OS, Helper->LeafValues[MaskIdx]);
+    OS << " * ";
+    Mask -= (1ULL << MaskIdx);
+  }
+  MaskIdx = llvm::countTrailingZeros(Mask);
+  printValue(OS, Helper->LeafValues[MaskIdx]);
+}
+
+void LogicalOpNode::print(raw_ostream &OS) const {
+  OS << *Val << " --> ";
+  if (Expr.size() == 0) {
+    OS << "0\n";
+    return;
+  }
+
+  for (auto I = ++Expr.begin(); I != Expr.end(); I++) {
+    printAndChain(OS, *I);
+    OS << " + ";
+  }
+
+  printAndChain(OS, *Expr.begin());
+  OS << "\n";
+}
+
+void LogicalOpsHelper::clear() {
+  for (auto node : LogicalOpNodes)
+    delete node.second;
+  LogicalOpNodes.clear();
+  LeafSet.clear();
+  LeafValues.clear();
+}
+
+LogicalOpNode *LogicalOpsHelper::visitLeafNode(Value *Val, unsigned Depth) {
+  // Depth is 0 means the root is not logical operation. We can't
+  // do anything for that.
+  if (Depth == 0 || LeafSet.size() > MaxLogicOpLeafsToScan)
+    return nullptr;
+
+  uint64_t ExprVal = 1ULL << LeafSet.size();
+  // Constant Zero,AllOne are special leaf nodes. They involve
+  // LogicalExpr's calculation so we must detect them at first.
+  if (auto ConstVal = dyn_cast<ConstantInt>(Val)) {
+    if (ConstVal->isZero())
+      ExprVal = LogicalExpr::ExprZero;
+    else if (ConstVal->isAllOnesValue())
+      ExprVal = LogicalExpr::ExprAllOne;
+  }
+  if (ExprVal != LogicalExpr::ExprAllOne && ExprVal != LogicalExpr::ExprZero &&
+      LeafSet.insert(Val).second)
+    LeafValues.push_back(Val);
+  LogicalOpNode *Node = new LogicalOpNode(this, Val, LogicalExpr(ExprVal));
+  LogicalOpNodes[Val] = Node;
+  return Node;
+}
+
+LogicalOpNode *LogicalOpsHelper::visitBinOp(BinaryOperator *BO,
+                                            unsigned Depth) {
+  // We can only to simpilfy and, or , xor in the binary operator
+  if (BO->getOpcode() != Instruction::And &&
+      BO->getOpcode() != Instruction::Or && BO->getOpcode() != Instruction::Xor)
+    return visitLeafNode(BO, Depth);
+
+  LogicalOpNode *LHS = getLogicalOpNode(BO->getOperand(0), Depth + 1);
+  if (LHS == nullptr)
+    return nullptr;
+
+  LogicalOpNode *RHS = getLogicalOpNode(BO->getOperand(1), Depth + 1);
+  if (RHS == nullptr)
+    return nullptr;
+
+  LogicalOpNode *Node;
+  if (BO->getOpcode() == Instruction::And)
+    Node = new LogicalOpNode(this, BO, LHS->getExpr() & RHS->getExpr());
+  else if (BO->getOpcode() == Instruction::Or)
+    Node = new LogicalOpNode(this, BO, LHS->getExpr() | RHS->getExpr());
+  else
+    Node = new LogicalOpNode(this, BO, LHS->getExpr() ^ RHS->getExpr());
+  LogicalOpNodes[BO] = Node;
+  return Node;
+}
+
+LogicalOpNode *LogicalOpsHelper::getLogicalOpNode(Value *Val, unsigned Depth) {
+  if (Depth == MaxDepthLogicOpsToScan)
+    return nullptr;
+
+  if (LogicalOpNodes.find(Val) == LogicalOpNodes.end()) {
+    LogicalOpNode *Node;
+
+    // TODO: add select instruction support
+    if (auto *BO = dyn_cast<BinaryOperator>(Val))
+      Node = visitBinOp(BO, Depth);
+    else
+      Node = visitLeafNode(Val, Depth);
+
+    if (!Node)
+      return nullptr;
+    LLVM_DEBUG(dbgs() << *Node);
+  }
+  return LogicalOpNodes[Val];
+}
+
+Value *LogicalOpsHelper::logicalOpToValue(LogicalOpNode *Node) {
+  const LogicalExpr &Expr = Node->getExpr();
+  // Empty happen when all masks are earsed from the set because of  a ^ a = 0.
+  if (Expr.size() == 0)
+    return Constant::getNullValue(Node->getValue()->getType());
+
+  if (Expr.size() == 1) {
+    uint64_t ExprMask = *Expr.begin();
+    // ExprZero/ExprAllOne is not in the LeafValues
+    if (ExprMask == LogicalExpr::ExprZero)
+      return Constant::getNullValue(Node->getValue()->getType());
+    if (ExprMask == LogicalExpr::ExprAllOne)
+      return Constant::getAllOnesValue(Node->getValue()->getType());
+
+    if (llvm::popcount(ExprMask) == 1)
+      return LeafValues[llvm::Log2_64(ExprMask)];
+  }
+
+  // TODO: complex pattern simpilify
+
+  return nullptr;
+}
+
+Value *LogicalOpsHelper::simplify(Value *Root) {
+  LogicalOpNode *RootNode = getLogicalOpNode(Root);
+  if (RootNode == nullptr)
+    return nullptr;
+
+  Value *NewRoot = logicalOpToValue(RootNode);
+  if (NewRoot == Root)
+    return nullptr;
+
+  if (NewRoot != nullptr)
+    NumComplexLogicalOpsSimplified++;
+  return NewRoot;
+}
---
+++ llvm/lib/Transforms/AggressiveInstCombine/LogicalExpr.h
@@ -0,0 +1,124 @@
+//===------------------- LogicalExpr.h --------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// A example for the LogicalExpr caculation:
+// There are for sources a, b, c, d, the mask for them is 1, 2, 4, 8
+// LHS is (a * b * c * d + a * d + b + a * c * d), RHS is (a + a * c).
+// Use bit mask to represent the expression:
+// {0b1111, 0b1001, 0b0010 , 0b1101} * {0b0001, 0b0101}
+// -->
+// {0b1111 | 0b0001, 0b1001| 0b0001, 0b0010 | 0b0001, 0b1101 | 0b0001 } +
+// {0b1111 | 0b0101, 0b1001| 0b0101, 0b0010 | 0b0101, 0b1101 | 0b0101 }
+// -->
+// {0b1111, 0b1001, 0b0010, 0b1101} + {0b1111, 0b1101, 0b0111, 0b1101}
+// -->
+// {0b1001, 0b0010, 0b1101, 0b0111}
+// -->
+// a * d + b + a * c * d + a * b * c
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/ADT/DenseSet.h"
+
+namespace llvm {
+typedef SmallDenseSet<uint64_t, 8> ExprAddChain;
+
+class LogicalExpr {
+private:
+  ExprAddChain AddChain;
+  uint64_t LeafMask;
+
+  inline void updateLeafMask() {
+    LeafMask = 0;
+    for (auto Mask : AddChain)
+      LeafMask |= Mask;
+  }
+
+public:
+  static const uint64_t ExprAllOne = 0x8000000000000000;
+  static const uint64_t ExprZero = 0x4000000000000000;
+
+  LogicalExpr() {}
+  LogicalExpr(uint64_t Mask) {
+    AddChain.insert(Mask);
+    LeafMask = Mask;
+  }
+  LogicalExpr(const ExprAddChain &SrcAddChain) : AddChain(SrcAddChain) {
+    updateLeafMask();
+  }
+
+  unsigned size() const { return AddChain.size(); }
+  ExprAddChain::iterator begin() { return AddChain.begin(); }
+  ExprAddChain::iterator end() { return AddChain.end(); }
+  ExprAddChain::const_iterator begin() const { return AddChain.begin(); }
+  ExprAddChain::const_iterator end() const { return AddChain.end(); }
+
+  LogicalExpr &operator*=(const LogicalExpr &RHS) {
+    ExprAddChain NewChain;
+    for (auto LHSMask : AddChain) {
+      // a & 0 -> 0
+      if (LHSMask & ExprZero)
+        continue;
+      for (auto RHSMask : RHS.AddChain) {
+        // 0 & a -> 0
+        if (RHSMask & ExprZero)
+          continue;
+        uint64_t NewMask = LHSMask | RHSMask;
+        // a & 1 -> a
+        if (NewMask != ExprAllOne && ((NewMask & ExprAllOne) != 0))
+          NewMask &= ~ExprAllOne;
+        // a ^ a -> 0
+        if (!NewChain.insert(NewMask).second)
+          NewChain.erase(NewMask);
+      }
+    }
+
+    AddChain = NewChain;
+    updateLeafMask();
+    return *this;
+  }
+
+  LogicalExpr &operator+=(const LogicalExpr &RHS) {
+    for (auto RHSMask : RHS.AddChain) {
+      // a ^ a -> 0
+      if (!AddChain.insert(RHSMask).second)
+        AddChain.erase(RHSMask);
+    }
+    updateLeafMask();
+    return *this;
+  }
+};
+
+inline LogicalExpr operator*(LogicalExpr a, const LogicalExpr &b) {
+  a *= b;
+  return a;
+}
+
+inline LogicalExpr operator+(LogicalExpr a, const LogicalExpr &b) {
+  a += b;
+  return a;
+}
+
+inline LogicalExpr operator&(const LogicalExpr &a, const LogicalExpr &b) {
+  return a * b;
+}
+
+inline LogicalExpr operator^(const LogicalExpr &a, const LogicalExpr &b) {
+  return a + b;
+}
+
+inline LogicalExpr operator|(const LogicalExpr &a, const LogicalExpr &b) {
+  return a * b + a + b;
+}
+
+inline LogicalExpr operator~(const LogicalExpr &a) {
+  LogicalExpr AllOneExpr(LogicalExpr::ExprAllOne);
+  return a + AllOneExpr;
+}
+
+} // namespace llvm
